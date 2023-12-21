1a98e87fd1ed1701fa0ae64dee874e608729f9d8
李宣佑 <3B032106@gm.student.ncut.edu.tw>
2023-12-21 14:34:40 +0800

    在專案中新增相關所需的 Model、Controller、Migrations 等 All-In-One 程式碼

---
+++ app/Http/Controllers/MembersFriendController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MembersFriend;
+use App\Http\Requests\StoreMembersFriendRequest;
+use App\Http\Requests\UpdateMembersFriendRequest;
+
+class MembersFriendController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMembersFriendRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MembersFriend $membersFriend)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MembersFriend $membersFriend)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMembersFriendRequest $request, MembersFriend $membersFriend)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MembersFriend $membersFriend)
+    {
+        //
+    }
+}
---
+++ database/seeders/MembersFriendSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MembersFriendSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
