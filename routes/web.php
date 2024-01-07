50ee5e81dfd21aff92ba0b6fc30679e188e86f69
李宣佑 <3B032106@gm.student.ncut.edu.tw>
2024-01-07 16:10:13 +0800

    修改 web.php 其相對的路由名稱、新增相對的路徑。

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,17 @@ Route::get('/', [ProductController::class, 'index'])->name('products.index');
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart/store', [CartController::class, 'store'])->name('cart.store');
 
-Route::get('/order', [OrderController::class, 'index'])->name('order');
+Route::get('/order', [OrderController::class, 'index'])->name('order.index');
+Route::get('/order/{order}/edit', [OrderController::class, 'edit'])->name('order.edit');
 
-Route::post('/order/store', [OrderController::class, 'store'])->name('order.store');
+Route::post('/order', [OrderController::class, 'store'])->name('order.store');
+Route::put('/order/{order}/update', [OrderController::class, 'update'])->name('order.update');
+Route::delete('/order/{order}', [OrderController::class, 'destroy'])->name('order.destroy');
+
+
+Route::put('/pay/{order}', [OrderController::class, 'pay'])->name('order.pay');
+Route::get('/orderdetail/show/{order}', [OrderController::class, 'showOrderDetail'])->name('order.detail.show');
+
 
 Route::middleware('auth')->group(function () {
     // 使用新的 MemberController 來更新個人資料
@@ -46,6 +54,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('friend', FriendController::class);
+
 /*
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
